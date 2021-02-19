a50da72df4618b4ff6509a3f83a540dd28901dda
SandrineZ3 <79086955+SandrineZ3@users.noreply.github.com>
2021-02-19 11:50:30 +0100

    re

--- src/fr/eni/encheres/bo/Utilisateur.java
+++ src/fr/eni/encheres/bo/Utilisateur.java
@@ -15,10 +15,29 @@ public class Utilisateur {
 	private int credit;
 	private boolean administrateur; // Si oui ou non est administrateur
 	
+	
+	// Constructeur complet 
+	public Utilisateur(int noUtilisateur, String pseudo, String nom, String prenom, String email, String telephone, String rue, String codePostal, String ville, String motDePasse, int credit, boolean administrateur) {
+		this.noUtilisateur = noUtilisateur;
+		this.pseudo = pseudo;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.telephone = telephone;
+		this.rue = rue;
+		this.codePostal = codePostal;
+		this.ville = ville;
+		this.motDePasse = motDePasse;
+		this.credit = credit;
+		this.administrateur = administrateur;
+	}
+	
 	// Constructeur sans paramètres
 	public Utilisateur() {
 		
 	}
+	
+	
 	
 	
 	// Getter et Setter
